2e40aa8336ad82eadf5007bd315d58d74271eba5
Jean Cardo <jean.cardo@digitalhouse.com>
2024-09-08 20:36:29 -0300

    [scaffolding] built initial scaffolding for go app

---
+++ cmd/handler/globalHandler.go
@@ -0,0 +1,15 @@
+package handler
+
+type GlobalHandler struct {
+	// Add fields here
+}
+
+func NewGlobalHandler() *GlobalHandler {
+	return &GlobalHandler{
+		// Initialize fields here
+	}
+}
+
+func (gh *GlobalHandler) BuildRoutes() {
+	// Add routes here
+}
